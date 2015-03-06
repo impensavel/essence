65cd63973cae44348d0058006e7d5edc8c3b39db
Quetzy Garcia <qgarcia@wearearchitect.com>
2015-03-06 10:37:16 +0000

    fix(*): add markdown extension

--- src/XMLEssence.php
+++ src/XMLEssence.php
@@ -6,7 +6,7 @@
  * @copyright  2014-2015
  *
  * For the full copyright and license information,
- * please view the LICENSE file that was distributed
+ * please view the LICENSE.md file that was distributed
  * with this source code.
  */
 

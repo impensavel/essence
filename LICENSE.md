eb1a85a70a036883465b3f3bccc2ca071a5d1eab
Quetzy Garcia <qgarcia@golin.com>
2016-03-14 14:52:29 +0000

    chore(*): update to 2016

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 ### The MIT License (MIT)
 
-Copyright (c) 2014-2015 Quetzy Garcia
+Copyright (c) 2014-2016 Quetzy Garcia
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- src/XMLEssence.php
+++ src/XMLEssence.php
@@ -3,7 +3,7 @@
  * This file is part of the Essence library.
  *
  * @author     Quetzy Garcia <quetzyg@impensavel.com>
- * @copyright  2014-2015
+ * @copyright  2014-2016
  *
  * For the full copyright and license information,
  * please view the LICENSE.md file that was distributed
